5837c1629b89ece1cec16bbe5f6781917bd902a1
Nithin U J <girishnithin616@gmail.com>
2024-07-06 00:19:54 +0530

    address_build

--- frontend/src/Address.jsx
+++ frontend/src/Address.jsx
@@ -1,3 +1,4 @@
 // export const address = "http://localhost:5000";
 // export const address = "http://192.168.203.194:5000";
-export const address = "http://65.2.132.52:5000";
+// export const address = "http://65.2.132.52:5000";
+export const address = "http://edugiri.cloud:5000";
