6368168623cbd7544806b1310a3bbd7464b6abb5
Nithin U J <girishnithin616@gmail.com>
2024-07-05 23:19:56 +0530

    address

--- frontend/src/Address.jsx
+++ frontend/src/Address.jsx
@@ -1,3 +1,3 @@
 // export const address = "http://localhost:5000";
-export const address = "http://192.168.203.194:5000";
 // export const address = "http://192.168.203.194:5000";
+export const address = "http://65.2.132.52:5000";
